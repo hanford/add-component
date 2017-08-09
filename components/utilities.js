7f706f2bfe5f73d17a881fb0ab74d4aae1057937
Evan <evan@crowdhubapps.com>
2017-08-09 14:40:50 -0700

    Fixing typo while refactoring the utilities file.

--- components/utilities.js
+++ components/utilities.js
@@ -8,8 +8,8 @@ function getTemplate(dir, name, css, templateName) {
   const file = path.join(dir, `${name}.js`);
 
   const templateLocation = css
-    ? `templates/${templateName}-css.js`
-    : `templates/${templateName}.js`;
+    ? `../templates/${templateName}-css.js`
+    : `../templates/${templateName}.js`;
 
   const body = fs
     .readFileSync(path.join(__dirname, templateLocation), "utf-8")
@@ -20,14 +20,14 @@ function getTemplate(dir, name, css, templateName) {
 }
 
 function getStyles(dir) {
-  const templateLocation = "templates/style.css";
+  const templateLocation = "../templates/style.css";
   const body = fs.readFileSync(path.join(__dirname, templateLocation), "utf-8");
   fs.writeFileSync(path.join(dir, "style.css"), body);
 }
 
 function getTest(dir, name) {
   const file = path.join(dir, `${name}.test.js`);
-  const templateLocation = "templates/test.js";
+  const templateLocation = "../templates/test.js";
 
   const body = fs
     .readFileSync(path.join(__dirname, templateLocation), "utf-8")
@@ -41,7 +41,7 @@ function getTest(dir, name) {
 
 function getIndex(dir, name) {
   const file = path.join(dir, "index.js");
-  const templateLocation = "templates/index.js";
+  const templateLocation = "../templates/index.js";
 
   const body = fs
     .readFileSync(path.join(__dirname, templateLocation), "utf-8")
