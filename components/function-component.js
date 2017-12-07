76aa6cb3272c85380c6dff9a74db03f67b919902
Varya Stepanova <mail@varya.me>
2017-12-07 20:19:50 +0200

    Provide option to write component main content into index.js

--- components/function-component.js
+++ components/function-component.js
@@ -2,6 +2,6 @@ const getTemplate = require('./get-template')
 
 module.exports = PureComponent
 
-function PureComponent (rootDirectory, name, hasCSS) {
-  return getTemplate(rootDirectory, name, hasCSS, 'functional-component')
+function PureComponent (rootDirectory, name, hasCSS, file) {
+  return getTemplate(rootDirectory, name, hasCSS, 'functional-component', file)
 }
